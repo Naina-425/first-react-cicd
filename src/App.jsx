48bf9d1f3f819d60e7b8754eb7ce64197cdaacb7
naina-425 <naina.manocha45@gmail.com>
2025-05-01 20:33:17 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+   
+  <div>
+    <h1>My react app</h1>
+    <div>Practising cicd pipeline</div>
+  </div>
+  )
+}
+
+export default App
